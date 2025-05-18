297f03be5a46f68268b0488b180120eb3e8c297f
MadushanLSKK <Kmadushan396@gmail.com>
2025-05-18 18:54:41 +0530

    splash screen bg image and logo has been created

--- app/src/main/java/com/example/fotpulse/MainActivity.java
+++ app/src/main/java/com/example/fotpulse/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.fotpulse;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static int SPLASH_TIMER=3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( );
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIMER);
+
     }
 }
