f74307c86feee1a98b3834be782ab2825b0a51e4
MadushanLSKK <Kmadushan396@gmail.com>
2025-06-03 01:11:44 +0530

    Intent Added for signup btn in signupActivity.java file

--- app/src/main/java/com/example/fotpulse/SignUpActivity.java
+++ app/src/main/java/com/example/fotpulse/SignUpActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -12,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class SignUpActivity extends AppCompatActivity {
     TextView login;
+    Button signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,15 +21,29 @@ public class SignUpActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
 
+
         login=(TextView) findViewById(R.id.loginText);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent( );
+                startActivity(intent);
 
 
             }
         });
+
+        signupBtn=(Button) findViewById(R.id.signup_btn);
+        signupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(  );
+                startActivity(intent);
+
+
+            }
+        });
+
 
     }
 }
