e238434d3d56f8fd0ffabd68a51f6f49ac170742
MadushanLSKK <Kmadushan396@gmail.com>
2025-06-09 22:08:36 +0530

    add function to sign up btn and edit info btn

--- app/src/main/java/com/example/fotpulse/UserInfoScreen.java
+++ app/src/main/java/com/example/fotpulse/UserInfoScreen.java
@@ -1,6 +1,10 @@
 package com.example.fotpulse;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,48 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class UserInfoScreen extends AppCompatActivity {
+    Button signOut;
+
+    Dialog dialog;
+    Button btnDialogCancel , btnDialogYes ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_info_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        signOut = findViewById(R.id.sign_out_button);
+
+        dialog= new Dialog(UserInfoScreen.this);
+        dialog.setContentView(R.layout.activity_user_info_screen2);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(getDrawable(R.drawable.custom_dialog_bg));
+        dialog.setCancelable(false);
+
+        btnDialogYes=dialog.findViewById(R.id.btnDialogYes);
+        btnDialogCancel=dialog.findViewById(R.id.btnDialogCancel);
+
+        btnDialogCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
         });
+
+        btnDialogYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+       signOut.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               dialog.show();
+           }
+       });
+
     }
 }
